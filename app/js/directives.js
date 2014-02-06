84213220419736aaf0bd001328a0919fcb2978f1
Pierre Guilbert <guilbep@gmail.com>
2014-02-06 22:38:18 +0100

    a

--- app/js/directives.js
+++ app/js/directives.js
@@ -23,7 +23,10 @@ angular.module('myApp.directives', [])
         console.log(scope.chicken);
         
       $swipe.bind(elm, function(start, move, end, cancel){
-        console.log(arguments);
+        
+          elm.css('top', end.y +'px');
+          elm.css('left', end.x+'px');
+
       });
         scope.$watch("chicken", function(newv) {
           console.log('NEW CHICKEN');
