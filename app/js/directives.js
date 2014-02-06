2bb7a80df127d27349d54f549cc6fc87e7481d00
Pierre Guilbert <guilbep@gmail.com>
2014-02-06 22:12:47 +0100

    a

--- app/js/directives.js
+++ app/js/directives.js
@@ -16,7 +16,7 @@ angular.module('myApp.directives', [])
         'chicken' : '=chicken'
       },
   		link : function(scope, elm, attrs, ctrl){
-  			elm.text(scope.getText())
+  			// elm.text(scope.getText())
   			elm.css('background-color', 'red');
 
         console.log('kikooo');
